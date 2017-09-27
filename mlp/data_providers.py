1b2f63cad3f92d228859282af4306aade282285f
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2017-09-27 15:44:58 +0100

    completed first two exercises

--- mlp/data_providers.py
+++ mlp/data_providers.py
@@ -133,11 +133,11 @@ class MNISTDataProvider(DataProvider):
         super(MNISTDataProvider, self).__init__(
             inputs, targets, batch_size, max_num_batches, shuffle_order, rng)
 
-    # def next(self):
-    #    """Returns next data batch or raises `StopIteration` if at end."""
-    #    inputs_batch, targets_batch = super(MNISTDataProvider, self).next()
-    #    return inputs_batch, self.to_one_of_k(targets_batch)
-    #
+    def next(self):
+       """Returns next data batch or raises `StopIteration` if at end."""
+       inputs_batch, targets_batch = super(MNISTDataProvider, self).next()
+       return inputs_batch, self.to_one_of_k(targets_batch)
+    
     def __next__(self):
         return self.next()
 
@@ -156,7 +156,15 @@ class MNISTDataProvider(DataProvider):
             to zero except for the column corresponding to the correct class
             which is equal to one.
         """
-        raise NotImplementedError()
+        num_data = len(int_targets)
+        num_classes = max(int_targets) + 1
+        
+        # create an array of size (num_data, num_classes):
+        encoded = np.zeros((num_data, num_classes))
+        
+        for i, num in enumerate(int_targets):
+            encoded[i,num] = 1
+        return encoded
 
 
 class MetOfficeDataProvider(DataProvider):
