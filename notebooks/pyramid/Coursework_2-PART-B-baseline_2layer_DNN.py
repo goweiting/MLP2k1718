dd69866f2e1c5280d88ff9ffc0829ad4c3db4fac
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2017-11-25 22:21:01 +0000

    uploading data

--- notebooks/pyramid/Coursework_2-PART-B-baseline_2layer_DNN.py
+++ notebooks/pyramid/Coursework_2-PART-B-baseline_2layer_DNN.py
@@ -1,55 +1,9 @@
 from __future__ import print_function
 import pickle as pkl
-
-
-def train_model_and_plot_stats(model,
-                               error,
-                               learning_rule,
-                               train_data,
-                               valid_data,
-                               test_data,
-                               num_epochs,
-                               stats_interval,
-                               notebook=False,
-                               earlyStopping=True):
-    # As well as monitoring the error over training also monitor classification
-    # accuracy i.e. proportion of most-probable predicted classes being equal to targets
-    data_monitors = {'acc': lambda y, t: (y.argmax(-1) == t.argmax(-1)).mean()}
-
-    # Use the created objects to initialise a new Optimiser instance.
-    if earlyStopping:
-        optimiser = EarlyStoppingOptimiser(
-            model,
-            error,
-            learning_rule,
-            train_data,
-            valid_data,
-            test_data,
-            data_monitors,
-            notebook=notebook,
-            steps=3,
-            patience=3)
-        stats, key, run_time, best_epoch = optimiser.train(
-            max_num_epochs=num_epochs, stats_interval=stats_interval)
-        return (optimiser, stats, key, run_time, best_epoch)
-    else:
-        optimiser = Optimiser(
-            model,
-            error,
-            learning_rule,
-            train_data,
-            valid_data,
-            test_data,
-            data_monitors,
-            notebook=notebook)
-
-        stats, keys, run_time = optimiser.train(
-            num_epochs=num_epochs, stats_interval=stats_interval)
-        return (optimiser, stats, keys, run_time)
-
 import numpy as np
 import logging
-from mlp.data_providers import MNISTDataProvider, EMNISTDataProvider
+from mlp.helper import train_model_and_plot_stats
+from mlp.data_providers import EMNISTDataProvider
 
 # Seed a random number generator
 seed = 10102016
@@ -75,10 +29,11 @@ from mlp.models import MultipleLayerModel
 from mlp.initialisers import ConstantInit, GlorotUniformInit
 from mlp.learning_rules import GradientDescentLearningRule, AdamLearningRule, RMSPropLearningRule
 from mlp.optimisers import Optimiser, EarlyStoppingOptimiser
+from mlp.penalty import *
 
 # setup hyperparameters
 learning_rate = 0.1
-num_epochs = 300
+num_epochs = 100
 stats_interval = 1
 input_dim, output_dim, hidden_dim = 784, 47, 100
 
@@ -91,7 +46,7 @@ weights_init = GlorotUniformInit(rng=rng)
 biases_init = ConstantInit(0.)
 model = MultipleLayerModel([
     ReshapeLayer(output_shape=(1, 28, 28)),
-    ConvolutionalLayer(
+    ConvolutionalLayer_NUMBA(
         num_input_channels=1,
         num_output_channels=5,
         input_dim_1=28,
@@ -100,12 +55,12 @@ model = MultipleLayerModel([
         kernel_dim_2=5,
         padding=0,
         stride=1),
-    MaxPoolingLayer(
+    MaxPoolingLayer_NUMBA(
         num_input_channels=5, input_dim_1=24, input_dim_2=24, extent=2),
     ReshapeLayer(output_shape=(12 * 12 * 5,)),
     ReluLayer(),
     ReshapeLayer(output_shape=(5,12,12)),
-    ConvolutionalLayer(
+    ConvolutionalLayer_NUMBA(
         num_input_channels=5,
         num_output_channels=10,
         input_dim_1=12,
@@ -114,7 +69,7 @@ model = MultipleLayerModel([
         kernel_dim_2=5,
         padding=0,
         stride=1),
-    MaxPoolingLayer(
+    MaxPoolingLayer_NUMBA(
         num_input_channels=10, input_dim_1=8, input_dim_2=8, extent=2),
     ReshapeLayer(output_shape=(4 * 4 * 10,)),
     AffineLayer(4*4*10, 400, weights_init, biases_init),
@@ -132,7 +87,7 @@ error = CrossEntropyLogSoftmaxError()
 learning_rule = AdamLearningRule()
 
 # Remember to use notebook=False when you write a script to be run in a terminal
-optimiser, stats, key, run_time = train_model_and_plot_stats(
+output = train_model_and_plot_stats(
     model,
     error,
     learning_rule,
@@ -142,15 +97,5 @@ optimiser, stats, key, run_time = train_model_and_plot_stats(
     num_epochs,
     stats_interval,
     notebook=False,
-    earlyStopping=False)
-_model = optimiser.model
-try:
-    pkl.dump((stats, key, run_time), open('BASELINE_2layer_4DNN.{}.pkl'.format(trial), 'wb'), protocol=-1)
-except:
-    print("STATS\n",run_time)
-    print("STATS\n",stats)
-    print("KEYS\n", key)
-try:
-    pkl.dump(_model, open('BASELINE_2layer_4DNN.{}.model.pkl'.format(trial), 'wb'), portocol=-1)
-except:
-    print(_model)
+    displayGraphs=False)
+pkl.dump(output, open('BASELINE_2layer_4DNN.{}.pkl'.format(trial), 'wb'), protocol=-1)
