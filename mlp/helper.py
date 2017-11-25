601f4bed1f0d7fb5d2b0ba29d7a2ca415004e7e2
goweiting <gwt.543@gmail.com>
2017-11-25 14:22:27 +0000

    moving to gCloud

--- mlp/helper.py
+++ mlp/helper.py
@@ -67,3 +67,59 @@ def train_model_and_plot_stats(model,
         return optimiser.model, stats, keys, run_time, fig_1, ax_1, fig_2, ax_2
     else:
         return optimiser.model, stats, keys, run_time
+    
+def analyse_mean_std(data):
+    """
+    Find the best mean
+    """
+    best_epochs = {}
+    for i in data.keys():
+        # each parameter's validation and training result
+        _data = data[i]
+        # plot the accuracy for each layer
+        _tr_acc = _data['train_acc']
+        _tr_err = _data['train_err']
+        _val_acc = _data['val_acc']
+        _val_err = _data['val_err']
+        _test_acc = _data['test_acc']
+        best_epoch = np.argmax(_val_acc['mean'])
+        best_epochs[i] = {'idx': best_epoch,
+                       'mean': _val_acc['mean'][best_epoch],
+                       'std':  _val_acc['std'][best_epoch],
+                         'test_acc_mean': _test_acc['mean'][best_epoch],
+                         'test_acc_std': _test_acc['std'][best_epoch]}
+    return best_epochs
+
+def generate_mean_std(stats):
+    """
+    stats ia dictionary of:
+    keys : parameters being varied
+    values: dataset
+    """
+    out = {}
+    for param in stats.keys():
+        # Iterate each parameter to generate stats for each and every epoch:
+        data = stats[param]
+        out[param] = {}
+        for _param in data.keys():
+            # this corresponds to the val_err/val_acc/train_acc/train_err:
+            _data = data[_param] # This data is for each trial!
+            _data_paded = numpy_fillna(_data)
+            mean = np.nanmean(_data_paded, axis=0)
+            std = np.nanstd(_data_paded, axis=0)
+            out[param][_param] = {'mean': mean,
+                                  'std' : std}
+    return out
+
+def numpy_fillna(data):
+    # Get lengths of each row of data
+    lens = np.array([len(i) for i in data])
+
+    # Mask of valid places in each row
+    mask = np.arange(lens.max()) < lens[:,None]
+
+    # Setup output array and put elements from data into masked positions
+    out = np.empty(mask.shape)
+    out[:] = np.nan
+    out[mask] = np.concatenate(np.array(data))
+    return out
