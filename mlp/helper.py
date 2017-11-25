0ac7b918902bbdee8ebf2a833cccbe17e2dc7092
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2017-11-25 19:39:29 +0000

    experiments on ottokar

--- mlp/helper.py
+++ mlp/helper.py
@@ -1,5 +1,4 @@
 ### HELPER FUNCTIONS
-
 from mlp.errors import CrossEntropyLogSoftmaxError
 from mlp.models import MultipleLayerModel
 from mlp.initialisers import *
@@ -9,65 +8,6 @@ from mlp.optimisers import Optimiser
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-
-def train_model_and_plot_stats(model,
-                               error,
-                               learning_rule,
-                               train_data,
-                               valid_data,
-                               test_data,
-                               num_epochs,
-                               stats_interval,
-                               notebook=False,
-                               displayGraphs=False):
-
-    # As well as monitoring the error over training also monitor classification
-    # accuracy i.e. proportion of most-probable predicted classes being equal to targets
-    data_monitors = {'acc': lambda y, t: (y.argmax(-1) == t.argmax(-1)).mean()}
-
-    # Use the created objects to initialise a new Optimiser instance.
-    optimiser = Optimiser(
-        model,
-        error,
-        learning_rule,
-        train_data,
-        valid_data,
-        test_data,
-        data_monitors,
-        notebook=notebook)
-
-    # Run the optimiser for 5 epochs (full passes through the training set)
-    # printing statistics every epoch.
-    stats, keys, run_time = optimiser.train(
-        num_epochs=num_epochs, stats_interval=stats_interval)
-
-    if displayGraphs:
-        # Plot the change in the validation and training set error over training.
-        fig_1 = plt.figure(figsize=(8, 4))
-        ax_1 = fig_1.add_subplot(111)
-        for k in ['error(train)', 'error(valid)']:
-            ax_1.plot(
-                np.arange(1, stats.shape[0]) * stats_interval,
-                stats[1:, keys[k]],
-                label=k)
-        ax_1.legend(loc=0)
-        ax_1.set_xlabel('Epoch number')
-
-        # Plot the change in the validation and training set accuracy over training.
-        fig_2 = plt.figure(figsize=(8, 4))
-        ax_2 = fig_2.add_subplot(111)
-        for k in ['acc(train)', 'acc(valid)']:
-            ax_2.plot(
-                np.arange(1, stats.shape[0]) * stats_interval,
-                stats[1:, keys[k]],
-                label=k)
-        ax_2.legend(loc=0)
-        ax_2.set_xlabel('Epoch number')
-
-        return optimiser.model, stats, keys, run_time, fig_1, ax_1, fig_2, ax_2
-    else:
-        return optimiser.model, stats, keys, run_time
-    
 def analyse_mean_std(data):
     """
     Find the best mean
@@ -123,3 +63,61 @@ def numpy_fillna(data):
     out[:] = np.nan
     out[mask] = np.concatenate(np.array(data))
     return out
+
+
+def train_model_and_plot_stats(model,
+                               error,
+                               learning_rule,
+                               train_data,
+                               valid_data,
+                               test_data,
+                               num_epochs,
+                               stats_interval,
+                               notebook=False,
+                               displayGraphs=False):
+
+    # As well as monitoring the error over training also monitor classification
+    # accuracy i.e. proportion of most-probable predicted classes being equal to targets
+    data_monitors = {'acc': lambda y, t: (y.argmax(-1) == t.argmax(-1)).mean()}
+
+    # Use the created objects to initialise a new Optimiser instance.
+    optimiser = Optimiser(
+        model,
+        error,
+        learning_rule,
+        train_data,
+        valid_data,
+        test_data,
+        data_monitors,
+        notebook=notebook)
+
+    # Run the optimiser for 5 epochs (full passes through the training set)
+    # printing statistics every epoch.
+    stats, keys, run_time = optimiser.train(num_epochs=num_epochs, stats_interval=stats_interval)
+
+    if displayGraphs:
+        # Plot the change in the validation and training set error over training.
+        fig_1 = plt.figure(figsize=(8, 4))
+        ax_1 = fig_1.add_subplot(111)
+        for k in ['error(train)', 'error(valid)']:
+            ax_1.plot(
+                np.arange(1, stats.shape[0]) * stats_interval,
+                stats[1:, keys[k]],
+                label=k)
+        ax_1.legend(loc=0)
+        ax_1.set_xlabel('Epoch number')
+
+        # Plot the change in the validation and training set accuracy over training.
+        fig_2 = plt.figure(figsize=(8, 4))
+        ax_2 = fig_2.add_subplot(111)
+        for k in ['acc(train)', 'acc(valid)']:
+            ax_2.plot(
+                np.arange(1, stats.shape[0]) * stats_interval,
+                stats[1:, keys[k]],
+                label=k)
+        ax_2.legend(loc=0)
+        ax_2.set_xlabel('Epoch number')
+
+        return optimiser.model, stats, keys, run_time, fig_1, ax_1, fig_2, ax_2
+    else:
+        return optimiser.model, stats, keys, run_time
