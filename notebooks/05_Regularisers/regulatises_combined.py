53a398dbabae92bc6d5aff9580f3e3558de937b8
goweiting <gwt.543@gmail.com>
2017-11-27 14:06:53 +0000

    ff for gcloud

--- notebooks/05_Regularisers/regulatises_combined.py
+++ notebooks/05_Regularisers/regulatises_combined.py
@@ -18,6 +18,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.handlers = [logging.StreamHandler()]
 # Create data provider objects for the MNIST data set
+batch_size = 200
 train_data = EMNISTDataProvider('train', batch_size=batch_size, rng=rng)
 valid_data = EMNISTDataProvider('valid', batch_size=batch_size, rng=rng)
 test_data = EMNISTDataProvider('test', batch_size=batch_size, rng=rng)
@@ -28,13 +29,11 @@ stats_interval = 1
 input_dim, output_dim, hidden_dim = 784, 47, 400
 i = 4  # + 1 = 5 layers
 
-
 # ==================================================================================
 # VARIABLES
 func = ELULayer()
-batch_sizes = [100]
 probs = [.5, .7, .9]
-weights_penalty = L2Penalty(1e-4)
+weight_penalty = L2Penalty(1e-4)
 # ==================================================================================
 for trial in [1,2,3]:
     experiment = {}
@@ -54,11 +53,11 @@ for trial in [1,2,3]:
             BatchNormalizationLayer(input_dim =hidden_dim, rng=rng)
         ]
         output_layer = [
-            ReluLayer(),
+            ELULayer(),
             AffineLayer(hidden_dim, output_dim, weights_init, biases_init, weights_penalty=weight_penalty) # NO BN TOWARDS SOFTMAX
         ]
         each_hidden_layer = [
-            ReluLayer(),
+            ELULayer(),
             DropoutLayer(rng=rng, incl_prob=incl_prob, share_across_batch=True),
             AffineLayerWithoutBias(hidden_dim, hidden_dim, weights_init, weights_penalty=weight_penalty),
             BatchNormalizationLayer(input_dim=hidden_dim, rng=rng) # BN AFTER OUTPUT == BN FOR INPUT TO NEXT LAYER            
@@ -67,7 +66,7 @@ for trial in [1,2,3]:
         # create the MLP:
         model = MultipleLayerModel(input_layer + each_hidden_layer * i + output_layer)
         error = CrossEntropyLogSoftmaxError()
-        learning_rule = GradientDescentLearningRule(learning_rate=.1)
+        learning_rule = GradientDescentLearningRule(learning_rate=.2)
 
         experiment[incl_prob] = train_model_and_plot_stats(model,
                                                            error,
@@ -83,4 +82,4 @@ for trial in [1,2,3]:
                                                            steps=3,
                                                            patience=5)
 
-    pkl.dump(experiment_BN, open('dropout_BN_l2_{}.pkl'.format(trial), 'wb'), protocol=-1)
+    pkl.dump(experiment, open('dropout_BN_l2_{}.pkl'.format(trial), 'wb'), protocol=-1)
